f48ea30f8e21bdedf8f85d7cd642a167181afbd0
Zach Moodie <dev@zachmoodie.com>
2024-10-22 17:16:50 -0600

    Adding cloud firestore and category mapping

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../config/firebase";
 
 import './Filter.css';
 
+async function fetchDataFromFirestore() {
+    const querySnapshot = await getDocs(collection(db, "categories"))
+
+    const data = [];
+    querySnapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data()});
+    });
+    return data.sort((a, b) => a.name.localeCompare(b.name));
+    
+}
+
 const Filter = () => {
+
+    const [categoryData, setCategoryData] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await fetchDataFromFirestore();
+            setCategoryData(data);
+        }
+        fetchData();
+        
+    }, []);
+
     return (
         <div id="filterWrap">
-            <ul className="filter">
-                <li className="filter-item active">All</li>
-                <li className="filter-item">Appetizers</li>
-                <li className="filter-item">Mains</li>
-                <li className="filter-item">Sides</li>
-                <li className="filter-item">Desserts</li>
-                <li className="filter-item">Breakfast</li>
-                <li className="filter-item">Lunch</li>
-                <li className="filter-item">Dinner</li>
-                <li className="filter-item">Soups</li>
-                <li className="filter-item">Salads</li>
-                <li className="filter-item">Dips</li>
-            </ul>
+            {categoryData[0] ? (
+                <ul className="filter categories">
+                    <li className="category">All</li>
+                    {categoryData.map((category, index) => (
+                            <li
+                                key={index}
+                                id={category.id}
+                                className="category"
+                            >{category.name}</li>
+                    ))}
+                </ul>
+            ) : (<></>)}
         </div>
     );
 };
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -14,6 +15,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+
+
+// Initialize Firestore
+export const db = getFirestore(app);
